793b37449ece8fdf8dec3e3b0d4257ad938d8472
Rodo <rodolfocabrera007@gmail.com>
2023-04-23 10:14:16 -0300

    lpm

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import Test from './components/testClaseBackend/Test';
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Test/> */}
        <App />
     </BrowserRouter>
   </React.StrictMode>
